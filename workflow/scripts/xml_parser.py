806c570dfed43571e882633a9646262816e9e97c
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2021-08-07 14:55:38 -0300

    Improve script 'xml_parser'
      - Add logging
      - Create main function
      - Add code for Snakemake usage
    
    
    Former-commit-id: 6ecfe8eb20e1537178dfd54ee38b4f38c028a862

--- workflow/scripts/xml_parser.py
+++ workflow/scripts/xml_parser.py
@@ -26,49 +26,73 @@ def search( node, query, parent_lineage=[] ):
 	return False
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument('lineage', help="The KEGG linage file.")
-parser.add_argument('xmls', help="The XML files to search for the hits.",  nargs='+')
-parser.add_argument('--outfile', help="The name of the output file. Default: OTU.tsv", default="OTU.tsv")
 
-args = parser.parse_args()
+def main(args):
+
+	with open(args.lineage) as f:
+		tax = json.load(f)
+
+	df = pd.DataFrame()
+
+	sample_names = []
+	for xml in args.xmls:
+		sample_name = xml.split(".")[0]
+		sample_names.append(sample_name)
+
+		root = ET.parse(xml).getroot()
+		for hit in root.findall("./BlastOutput_iterations/Iteration/Iteration_hits/Hit"):
+			hit_text = hit.find("Hit_id").text
+			code = hit_text.split(":")[0]
+			df = df.append( {'code':code, "sample_name":sample_name}, ignore_index=True)
+
+	print(f"Writing to {args.outfile}")
+	with open(args.outfile, "w") as f:
+		columns = ["Name", "Kegg Code", "Lineage"] + sample_names
+		print("\t".join(columns), file=f)
+
+		codes = sorted(df.code.unique())
+
+		for code in codes:
+			lineage = search( tax, code )
+			lineage.pop(0) # Get rid of root
+			lineage_text = "; ".join(lineage)
+			name = lineage[-1]
+			name = name[ len(code):].strip()
+
+			f.write( "\t".join( [name, code, lineage_text] )  )
+			for sample_name in sample_names:
+				filtered = df[ (df.sample_name == sample_name) & (df.code == code) ]
+				count = len(filtered.index)
+				f.write( f"\t{count}" )
+
+			f.write("\n")
 
 
-with open(args.lineage) as f:
-	tax = json.load(f)
+def run(snakemake):
+	args = argparse.Namespace()
+	args_dict = vars(args)
+
+	for rule_input in ("xmls",):
+		args_dict[rule_input] = snakemake.input[rule_input]
+
+	for rule_param in ("lineage",):
+		args_dict[rule_param] = snakemake.params[rule_param]
+
+	for rule_out in ("outputfile",):
+		args_dict[rule_output] = snakemake.output[rule_output]
+
+	main(args)
 
 
-df = pd.DataFrame()
-
-sample_names = []
-for xml in args.xmls:
-	sample_name = xml.split(".")[0]
-	sample_names.append(sample_name)
-
-	root = ET.parse(xml).getroot()
-	for hit in root.findall("./BlastOutput_iterations/Iteration/Iteration_hits/Hit"):
-		hit_text = hit.find("Hit_id").text
-		code = hit_text.split(":")[0]
-		df = df.append( {'code':code, "sample_name":sample_name}, ignore_index=True)
-
-print(f"Writing to {args.outfile}")
-with open(args.outfile, "w") as f:
-	columns = ["Name", "Kegg Code", "Lineage"] + sample_names
-	print("\t".join(columns), file=f)
-
-	codes = sorted(df.code.unique())
-
-	for code in codes:
-		lineage = search( tax, code )
-		lineage.pop(0) # Get rid of root
-		lineage_text = "; ".join(lineage)
-		name = lineage[-1]
-		name = name[ len(code):].strip()
-
-		f.write( "\t".join( [name, code, lineage_text] )  )
-		for sample_name in sample_names:
-			filtered = df[ (df.sample_name == sample_name) & (df.code == code) ]
-			count = len(filtered.index)
-			f.write( f"\t{count}" )
-
-		f.write("\n")
+# Logging config must be at the beginning
+logging.basicConfig(
+    filename=str(snakemake.log),
+    encoding="utf-8",
+    level=logging.DEBUG,
+    format="%(asctime)s %(message)s",
+    datefmt="%m/%d/%Y %H:%M:%S",
+)
+logging.info(f"Starting script {__file__.split('/')[-1]}.")
+logging.debug(f"Full script path: {__file__}")
+run(snakemake)
+logging.info(f"Done.")
