f1545483ec2cb20e6930e6d7128b3845e40bb0b2
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2021-08-07 17:32:36 -0300

    Update version of script 'xml_parser'
      The previous version xml_parser was based on v3 of the verbruggen branch of MGP.
      This version is based on the one at 'benchmark/taxon/xml_parser.py'. This version
      uses the dbm version of the taxonomic lineage database. It may be refactored in
      in the future.

--- workflow/scripts/xml_parser.py
+++ workflow/scripts/xml_parser.py
@@ -1,71 +1,57 @@
-import xml.etree.ElementTree as ET
-import sys
-import json
 import re
+import sys
+import shelve
+import logging
 import argparse
+import xml.etree.ElementTree as ET
+
 import pandas as pd
-
-
-def search( node, query, parent_lineage=[] ):
-	code = node["name"].split()[0]
-
-	# Append this node to the lineage
-	my_linage = list(parent_lineage) + [node["name"]]
-
-	if code == query:
-		return my_linage
-
-	# if it doesn't have the code, then search children
-	if "children" in node:
-		for child in node["children"]:
-			result = search( child, query, my_linage )
-			if result != False:
-				return result
-
-	# if no children then return false
-	return False
-
 
 
 def main(args):
 
-	with open(args.lineage) as f:
-		tax = json.load(f)
-
-	df = pd.DataFrame()
+	tax = shelve.open(args.db)
+	df = pd.DataFrame(columns=("code", "sample_name"))
 
 	sample_names = []
 	for xml in args.xmls:
-		sample_name = xml.split(".")[0]
+		sample_name = xml.split("/")[-1].split(".")[0]
 		sample_names.append(sample_name)
 
 		root = ET.parse(xml).getroot()
 		for hit in root.findall("./BlastOutput_iterations/Iteration/Iteration_hits/Hit"):
 			hit_text = hit.find("Hit_id").text
 			code = hit_text.split(":")[0]
-			df = df.append( {'code':code, "sample_name":sample_name}, ignore_index=True)
+			df = df.append( {"code":code, "sample_name":sample_name}, ignore_index=True)
 
-	print(f"Writing to {args.outfile}")
+	logging.info(f"Writing to {args.outfile}")
 	with open(args.outfile, "w") as f:
 		columns = ["Name", "Kegg Code", "Lineage"] + sample_names
-		print("\t".join(columns), file=f)
+		# import pdb; pdb.set_trace()
+		codes = sorted(df["code"].unique())
 
-		codes = sorted(df.code.unique())
+		if codes:
+			for code in codes:
+				if code in tax.keys():
+					lineage = tax[code]
+					name = lineage.split(";")[-1]
+					name = name[ len(code)+2:].strip()
+				else:
+					lineage = "unknown"
+					name = "Unknown"
 
-		for code in codes:
-			lineage = search( tax, code )
-			lineage.pop(0) # Get rid of root
-			lineage_text = "; ".join(lineage)
-			name = lineage[-1]
-			name = name[ len(code):].strip()
+				f.write( "\t".join( [name, code, lineage] )  )
+				for sample_name in sample_names:
+					filtered = df[ (df["sample_name"] == sample_name) & (df["code"] == code) ]
+					count = len(filtered.index)
+					f.write( f"\t{count}" )
 
-			f.write( "\t".join( [name, code, lineage_text] )  )
-			for sample_name in sample_names:
-				filtered = df[ (df.sample_name == sample_name) & (df.code == code) ]
-				count = len(filtered.index)
-				f.write( f"\t{count}" )
-
-			f.write("\n")
+				f.write("\n")
+		else:
+			input_files = "\n\t\t".join(args.xmls)
+			logging.debug(
+				f"The output appears to be empty. Please check the input files:\n{input_files}."
+			)
 
 
 def run(snakemake):
@@ -75,10 +61,10 @@ def run(snakemake):
 	for rule_input in ("xmls",):
 		args_dict[rule_input] = snakemake.input[rule_input]
 
-	for rule_param in ("lineage",):
+	for rule_param in ("db",):
 		args_dict[rule_param] = snakemake.params[rule_param]
 
-	for rule_out in ("outputfile",):
+	for rule_output in ("outfile",):
 		args_dict[rule_output] = snakemake.output[rule_output]
 
 	main(args)
