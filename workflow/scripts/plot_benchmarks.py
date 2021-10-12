02fa81de32627a728b55ed3cc8ea183ce431fc73
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2021-10-12 19:24:42 -0300

    Add shebang to script
    Former-commit-id: 418db93a0648b5641d167f2d8b75cead68978eeb

--- workflow/scripts/plot_benchmarks.py
+++ workflow/scripts/plot_benchmarks.py
@@ -1,3 +1,4 @@
+#!/usr/bin/env python
 import sys
 from pathlib import Path
 
@@ -19,15 +20,23 @@ def runtime_barplot_sum(df, outfile=None, time_unit="m", cutoff=5):
     Generates barplot of sum of rules' runtimes.
     """
     fig, ax = plt.subplots(figsize=(4, 8))
-    plot_data = (df.groupby(by=["module", "rule"])[time_unit].sum()).reset_index().sort_values(time_unit, ascending=False).query(f"{time_unit} > {cutoff}")
-    sns.barplot(x=time_unit, y="rule", hue="module", dodge=False, data=plot_data, palette="Dark2")
+    plot_data = (
+        (df.groupby(by=["module", "rule"])[time_unit].sum())
+        .reset_index()
+        .sort_values(time_unit, ascending=False)
+        .query(f"{time_unit} > {cutoff}")
+    )
+    sns.barplot(
+        x=time_unit,
+        y="rule",
+        hue="module",
+        dodge=False,
+        data=plot_data,
+        palette="Dark2",
+    )
     plt.legend(loc="lower right", title="Module")
 
-    xlabels = {
-        "h": "(hours)",
-        "m": "(minutes)",
-        "s": "(seconds)"
-    }
+    xlabels = {"h": "(hours)", "m": "(minutes)", "s": "(seconds)"}
 
     plt.title("Total rule runtime")
     ax.set_xlabel("Runtime " + xlabels[time_unit], fontsize=12)
@@ -43,20 +52,30 @@ def runtime_barplot_errorbar(df, outfile=None, time_unit="m", cutoff=1):
     The time-per-sample is corrected in rules that run once for all samples.
     """
     fig, ax = plt.subplots(figsize=(4, 8))
-    df[time_unit + "_corrected"] = df.apply(lambda row: row['m'] / df['sample'].str[:6].value_counts().shape[0] if not isinstance(row['sample'], str) else row['m'], axis=1)
-    plot_data = df.sort_values(time_unit + "_corrected", ascending=False).query(f"{time_unit} > {cutoff}")
-    sns.barplot(x=time_unit + "_corrected", y="rule", hue="module", dodge=False, data=plot_data, palette="Dark2")
+    df[time_unit + "_corrected"] = df.apply(
+        lambda row: row["m"] / df["sample"].str[:6].value_counts().shape[0]
+        if not isinstance(row["sample"], str)
+        else row["m"],
+        axis=1,
+    )
+    plot_data = df.sort_values(time_unit + "_corrected", ascending=False).query(
+        f"{time_unit} > {cutoff}"
+    )
+    sns.barplot(
+        x=time_unit + "_corrected",
+        y="rule",
+        hue="module",
+        dodge=False,
+        data=plot_data,
+        palette="Dark2",
+    )
     plt.legend(loc="lower right", title="Module")
 
-    xlabels = {
-        "h": "(hours)",
-        "m": "(minutes)",
-        "s": "(seconds)"
-    }
+    xlabels = {"h": "(hours)", "m": "(minutes)", "s": "(seconds)"}
 
     plt.title("Mean runtime per sample")
     ax.set_xlabel("Runtime " + xlabels[time_unit], fontsize=12)
-    ax.set_ylabel("Rule name", rotation=0, fontsize=12, position=(0, .7))
+    ax.set_ylabel("Rule name", rotation=0, fontsize=12, position=(0, 0.7))
     if outfile:
         plt.savefig(outfile, bbox_inches="tight")
 
@@ -66,21 +85,33 @@ def memory_barplot_sum(df, outfile=None, memory_unit="max_vms", cutoff=1, gb=Tru
     """
     Generates barplot of sum of rules' memory usage.
     """
-    if df[memory_unit].all() == '-':
+    if df[memory_unit].all() == "-":
         print("Skipping plot, no memory stats recorded.")
         return
     fig, ax = plt.subplots(figsize=(4, 8))
     df_ = df.copy()
     if gb:
-        df_[memory_unit] = df_[memory_unit] / 2**10
-    plot_data = (df_.groupby(by=["module", "rule"])[memory_unit].sum()).reset_index().sort_values(memory_unit, ascending=False).query(f"{memory_unit} > {cutoff}")
-    sns.barplot(x=memory_unit, y="rule", hue="module", dodge=False, data=plot_data, palette="Dark2")
+        df_[memory_unit] = df_[memory_unit] / 2 ** 10
+    plot_data = (
+        (df_.groupby(by=["module", "rule"])[memory_unit].sum())
+        .reset_index()
+        .sort_values(memory_unit, ascending=False)
+        .query(f"{memory_unit} > {cutoff}")
+    )
+    sns.barplot(
+        x=memory_unit,
+        y="rule",
+        hue="module",
+        dodge=False,
+        data=plot_data,
+        palette="Dark2",
+    )
     plt.legend(loc="lower right", title="Module")
 
     xlabels = {
         "max_vms": "Virtual Memory Size (MB)",
         "max_uss": "Unique Set Size (MB)",
-        "max_pss": "Proportional Set Size (MB)"
+        "max_pss": "Proportional Set Size (MB)",
     }
 
     plt.title("Sum of rules' memory usage")
@@ -97,21 +128,30 @@ def memory_barplot_errorbar(df, outfile=None, memory_unit="max_vms", cutoff=0, g
     """
     Generates barplot of sum of rules' memory usage.
     """
-    if df[memory_unit].all() == '-':
+    if df[memory_unit].all() == "-":
         print("Skipping plot, no memory stats recorded.")
         return
     fig, ax = plt.subplots(figsize=(4, 8))
     df_ = df.copy()
     if gb:
-        df_[memory_unit] = df_[memory_unit] / 2**10
-    plot_data = df_.query(f"{memory_unit} > {cutoff}").sort_values(memory_unit, ascending=False)
-    sns.barplot(x=memory_unit, y="rule", hue="module", dodge=False, data=plot_data, palette="Dark2")
+        df_[memory_unit] = df_[memory_unit] / 2 ** 10
+    plot_data = df_.query(f"{memory_unit} > {cutoff}").sort_values(
+        memory_unit, ascending=False
+    )
+    sns.barplot(
+        x=memory_unit,
+        y="rule",
+        hue="module",
+        dodge=False,
+        data=plot_data,
+        palette="Dark2",
+    )
     plt.legend(loc="lower right", title="Module")
 
     xlabels = {
         "max_vms": "Virtual Memory Size (MB)",
         "max_uss": "Unique Set Size (MB)",
-        "max_pss": "Proportional Set Size (MB)"
+        "max_pss": "Proportional Set Size (MB)",
     }
 
     plt.title("Mean memory usage per sample")
