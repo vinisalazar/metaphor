31d4c7309be509e8f99f1f87f0cf52c1be5664d1
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2021-12-16 20:11:20 +1100

    Fixing typo in scripts 'concatenate_benchmarks'

--- workflow/scripts/concatenate_benchmarks.py
+++ workflow/scripts/concatenate_benchmarks.py
@@ -65,4 +65,4 @@ if __name__ == "__main__":
     from utils import driver
     if "snakemake" not in locals():
         snakemake = None
-    driver(main, snakemak, __file__)
+    driver(main, snakemake, __file__)
