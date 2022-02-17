ebe607c6628021caf4d42fb91f7ee017ddd8a701
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-02-17 17:21:04 +1100

    Add args to 'metaphor test' command
      - Improve structure of module 'metaphor_test.py'

--- metaphor/cli/metaphor.py
+++ metaphor/cli/metaphor.py
@@ -14,22 +14,43 @@ Commands:
 
 import argparse
 
-from .metaphor_test import metaphor_test
+from .metaphor_test import main as metaphor_test
 
 
 def main():
     parser = argparse.ArgumentParser(prog="metaphor", description=__doc__)
     subparsers = parser.add_subparsers(help="Command to be executed.")
 
-    test = subparsers.add_parser("test", help="Test Metaphor with example data.")
+    test = subparsers.add_parser(
+        "test",
+        help="Test Metaphor with example data.",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter,
+    )
     test.add_argument("-d", "--directory", help="Directory to run tests.")
+    test.add_argument("-p", "--cores", help="Number of processors to use in tests.")
+    test.add_argument("-m", "--mem_mb", help="Amount of RAM to use in tests.")
+    test.add_argument(
+        "-c",
+        "--coassembly",
+        action="store_true",
+        help="Whether to run tests in coassembly mode, "
+        "i.e. all samples are pooled together and assembled.",
+    )
+    test.add_argument(
+        "-y",
+        "--confirm",
+        action="store_true",
+        help="Don't ask for confirmation when running tests.",
+    )
     test.add_argument(
         "--remove-conda",
         help="If this option is selected, conda environments will be created in the test "
         "directory instead of the current directory (and therefore are deleted when tests finish).",
         action="store_true",
     )
-    test.set_defaults(func=metaphor_test, directory="metaphor_test")
+    test.set_defaults(
+        func=metaphor_test, directory="metaphor_test", cores=4, mem_mb=4096
+    )
 
     config = subparsers.add_parser(
         "config", help="Shows the location of Metaphor installation and config values."
--- metaphor/cli/metaphor_test.py
+++ metaphor/cli/metaphor_test.py
@@ -12,6 +12,7 @@ __doc__ = """
     """
 
 
+import sys
 from argparse import Namespace
 from pathlib import Path
 from hashlib import md5
@@ -20,76 +21,84 @@ import requests
 from tqdm import tqdm
 from snakemake import snakemake
 
-from metaphor import snakefile
+from metaphor import snakefile, test_config, ascii_art
 from .create_input_table import main as create_input_table
 
 
-def metaphor_test(args):
-    directory = args.directory
-    test_files = {
-        "RL1_S001__insert_270_short_R1.fq.gz": "09a0e7403a9cea7c2d3c355db8c649cc",
-        "RL1_S001__insert_270_short_R2.fq.gz": "1c1178a5686422d9f5228bdc3a87f875",
-        "RL1_S002__insert_270_short_R1.fq.gz": "2ebfa1a25b3942bac2bda7f20dea97d7",
-        "RL1_S002__insert_270_short_R2.fq.gz": "6d1dfa872190e9bf3586328a81f307e3",
-        "RL2_S001__insert_270_short_R1.fq.gz": "9528b0e89ee769e236e720d2e8e71c33",
-        "RL2_S001__insert_270_short_R2.fq.gz": "66684a2b0b0a869a6016a8a615dd7932",
-        "RL2_S002__insert_270_short_R1.fq.gz": "0d85c5f8eb8fedc428e3804c3f8a0134",
-        "RL2_S002__insert_270_short_R2.fq.gz": "c46563a7b9d0e9be7cba475c5514f8b7",
-    }
-    download_url = "https://github.com/vinisalazar/mg-example-data/raw/main/data/"
+def get_md5(file):
+    with open(file, "rb") as f:
+        filehash = md5(f.read())
 
-    def get_md5(file):
-        with open(file, "rb") as f:
-            filehash = md5(f.read())
+    return filehash.hexdigest()
 
-        return filehash.hexdigest()
 
-    def download_file(url, filename):
-        """
-        Downloads a large file from a URL.
-        From: https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
-        """
+def download_file(url, filename):
+    """
+    Downloads a large file from a URL.
+    From: https://stackoverflow.com/questions/16694907/download-large-file-in-python-with-requests
+    """
 
-        # NOTE the stream=True parameter below
-        with requests.get(url, stream=True) as r:
-            r.raise_for_status()
-            # print(f"\tWriting to '{filename}'.")
-            with open(filename, "wb") as f:
-                for chunk in r.iter_content(chunk_size=8192):
-                    # If you have chunk encoded response uncomment if
-                    # and set chunk_size parameter to None.
-                    # if chunk:
-                    f.write(chunk)
-        return filename
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        # print(f"\tWriting to '{filename}'.")
+        with open(filename, "wb") as f:
+            for chunk in r.iter_content(chunk_size=8192):
+                # If you have chunk encoded response uncomment if
+                # and set chunk_size parameter to None.
+                # if chunk:
+                f.write(chunk)
+    return filename
 
-    def check_test_directory(directory):
-        directory = Path(directory)
-        if not (directory.exists() and directory.is_dir()):
-            print(f"Creating test directory: '{directory}'.")
-            directory.mkdir(exist_ok=True)
+
+def check_test_directory(directory):
+    directory = Path(directory)
+    if not (directory.exists() and directory.is_dir()):
+        print(f"Creating test directory: '{directory}'.")
+        directory.mkdir(exist_ok=True)
+    else:
+        print("Directory exists, skipping.")
+    return directory
+
+
+def download_data(directory, test_files, download_url):
+    test_directory = check_test_directory(directory)
+    print("Starting data download.")
+    downloaded, skipped = 0, 0
+    for filename, hexdigest in tqdm(test_files.items()):
+        local_file = test_directory.joinpath(filename)
+        if (not local_file.exists()) or (get_md5(local_file) != hexdigest):
+            url = download_url + filename
+            download_file(url, local_file)
+            downloaded += 1
         else:
-            print("Directory exists, skipping.")
-        return directory
+            # print(f"File '{filename}' exists and hash is correct. Skipping download.")
+            skipped += 1
 
-    def download_data(directory):
-        test_directory = check_test_directory(directory)
-        print("Starting data download.")
-        downloaded, skipped = 0, 0
-        for filename, hexdigest in tqdm(test_files.items()):
-            local_file = test_directory.joinpath(filename)
-            if (not local_file.exists()) or (get_md5(local_file) != hexdigest):
-                url = download_url + filename
-                download_file(url, local_file)
-                downloaded += 1
-            else:
-                # print(f"File '{filename}' exists and hash is correct. Skipping download.")
-                skipped += 1
+    for v in "downloaded", "skipped":
+        if eval(v):
+            msg = v.capitalize() + f" {eval(v)} files."
+            print(msg)
 
-        for v in "downloaded", "skipped":
-            if eval(v):
-                msg = v.capitalize() + f" {eval(v)} files."
-                print(msg)
 
+test_files = {
+    "RL1_S001__insert_270_short_R1.fq.gz": "09a0e7403a9cea7c2d3c355db8c649cc",
+    "RL1_S001__insert_270_short_R2.fq.gz": "1c1178a5686422d9f5228bdc3a87f875",
+    "RL1_S002__insert_270_short_R1.fq.gz": "2ebfa1a25b3942bac2bda7f20dea97d7",
+    "RL1_S002__insert_270_short_R2.fq.gz": "6d1dfa872190e9bf3586328a81f307e3",
+    "RL2_S001__insert_270_short_R1.fq.gz": "9528b0e89ee769e236e720d2e8e71c33",
+    "RL2_S001__insert_270_short_R2.fq.gz": "66684a2b0b0a869a6016a8a615dd7932",
+    "RL2_S002__insert_270_short_R1.fq.gz": "0d85c5f8eb8fedc428e3804c3f8a0134",
+    "RL2_S002__insert_270_short_R2.fq.gz": "c46563a7b9d0e9be7cba475c5514f8b7",
+}
+download_url = "https://github.com/vinisalazar/mg-example-data/raw/main/data/"
+
+
+def main(args):
+    directory = args.directory
+    coassembly = args.coassembly
+    cores = args.cores
+    mem_mb = args.mem_mb
     samples_file = str(Path(directory).joinpath("samples.csv"))
     create_input_table_args = Namespace()
     create_input_table_args.input_dir = directory
@@ -98,7 +107,7 @@ def metaphor_test(args):
     conda_prefix = directory if args.remove_conda else None
 
     # Start execution
-    download_data(directory)
+    download_data(directory, test_files, download_url)
     print("\nCreating input table for test files.\n")
     create_input_table(create_input_table_args)
     print()
@@ -106,16 +115,25 @@ def metaphor_test(args):
     print(
         "This may require the installation of conda environments which should take a while.\n"
     )
+    if not args.confirm:
+        confirm = input(
+            f"Snakemake will start with {cores} cores and {mem_mb} MB RAM. Ok to continue? [y/N]"
+        )
+        if confirm.lower() != "y":
+            print("Metaphor test cancelled.")
+            sys.exit()
+    print(ascii_art)
     snakemake(
         snakefile=snakefile,
         configfiles=[
-            "/Users/vwsalazar/Bio/phd/metaphor/config/test-config.yaml",
+            test_config,
         ],
         config={
             "samples": samples_file,
+            "coassembly": coassembly,
         },
-        cores=4,
-        resources={"mem_mb": 4096},
+        cores=cores,
+        resources={"mem_mb": mem_mb},
         use_conda=True,
         conda_prefix=conda_prefix,
         printshellcmds=True,
