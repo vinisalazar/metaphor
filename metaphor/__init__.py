bde36d47b8ce80526927c7b7c1df1465648c7c4c
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-02-17 17:20:18 +1100

    Add Metaphor ASCII art banner

--- metaphor/__init__.py
+++ metaphor/__init__.py
@@ -1,1 +1,14 @@
 from .workflow import snakefile
+from .config import test_config
+
+ascii_art = """
+   __  ___      __               __            
+  /  |/  /___  / /_ ___ _ ___   / /  ___   ____
+ / /|_/ // -_)/ __// _ `// _ \ / _ \/ _ \ / __/
+/_/  /_/ \__/ \__/ \_,_// .__//_//_/\___//_/   
+                       /_/                     
+
+Metaphor - Metagenomic Pipeline for Short Reads
+
+© The University of Melbourne 2022
+"""
