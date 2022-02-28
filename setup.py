411dafe6854fbffde6120e50a9c1a64e44f3361e
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-02-28 15:04:29 +1100

    Bump version and dependency
      - Add pyyaml as dependency

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ __author__ = "Vini Salazar"
 __email__ = "17276653+vinisalazar@users.noreply.github.com"
 __license__ = "MIT"
 __copyright__ = "2022, The University of Melbourne"
-__version__ = "0.4.0"
+__version__ = "0.5.0"
 
 from importlib.metadata import entry_points
 from pathlib import Path
@@ -33,7 +33,7 @@ setup(
     include_package_data=True,
     package_data={"metaphor": ["workflow/*", "workflow/*/*", "config/*"]},
     entry_points={"console_scripts": ["metaphor = metaphor.cli.metaphor:main"]},
-    install_requires=["pandas", "tqdm", "requests", "snakemake"],
+    install_requires=["pandas", "tqdm", "requests", "snakemake", "pyyaml"],
     keywords="metagenomics binning assembly snakemake workflow pipeline",
     python_requires=">=3.6",
     classifiers=[
