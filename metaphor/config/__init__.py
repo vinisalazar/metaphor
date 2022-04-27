29f412f38661c8c25460956bc1863a86f4c275e4
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-04-27 16:04:40 +1000

    Add example_input variable

--- metaphor/config/__init__.py
+++ metaphor/config/__init__.py
@@ -2,3 +2,4 @@ from pathlib import Path
 
 test_config = Path(__file__).parent.joinpath("test-config.yaml")
 default_config = Path(__file__).parent.joinpath("default-config.yaml")
+example_input = Path(__file__).parent.joinpath("samples.csv")
