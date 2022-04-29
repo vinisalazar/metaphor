ef9ec6eeb35d8f57a63e83ab090b221d429c8035
Vini Salazar <17276653+vinisalazar@users.noreply.github.com>
2022-04-29 15:03:42 +1000

    docs: add Alabaster theme settings

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,6 +51,13 @@ exclude_patterns = []
 #
 html_theme = "alabaster"
 
+html_theme_options = {
+    # "logo": "logo.png",
+    "logo_name": "true",
+    "github_user": "vinisalazar",
+    "github_repo": "metaphor",
+}
+
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
